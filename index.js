b44343467c61e6e7dab5f939d624db7f4f41d34e
Joe <36672081+jlh1186@users.noreply.github.com>
2021-10-04 23:31:39 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+const url = "https://www.theguardian.com/us"
+const articles = []
+axios(url)
+    .then(response => {
+        const html = response.data
+       // console.log(html)
+       const good = cheerio.load(html)
+       good('.fc-item__content', html).each(function(){
+            const title = good(this).text()
+          const url = good(this).find('a').attr('href')
+          articles.push({
+            title,
+            url
+          })
+       })
+       console.log(articles)
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log('Server Running on PORT:',PORT))
